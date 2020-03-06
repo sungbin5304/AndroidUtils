c91021c6bac41abb15ad2222a1aa9c421f298541
SungBin5304 <sungbin.dev@gmail.com>
2020-03-06 13:20:56 +0900

    Update

--- SungBinTool/src/main/java/com/sungbin/sungbintool/Utils.kt
+++ SungBinTool/src/main/java/com/sungbin/sungbintool/Utils.kt
@@ -10,7 +10,7 @@ import org.jsoup.Jsoup
 import java.io.IOException
 
 object Utils {
-    private const val USER_AGENT =
+    private var USER_AGENT =
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
 
     fun copy(ctx: Context, text: String) {
@@ -25,6 +25,10 @@ object Utils {
         ToastUtils.show(ctx, data, ToastUtils.LONG, ToastUtils.ERROR)
         copy(ctx, data)
         Log.e("Error", data)
+    }
+
+    fun setUserAgent(agent: String){
+        USER_AGENT = agent
     }
 
     fun getHtml(address: String): String? {
--- app/src/main/java/com/sungbin/sbt/MainActivity.kt
+++ app/src/main/java/com/sungbin/sbt/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sungbin.sbt
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sungbin.sungbintool.PermissionUtils
 import com.sungbin.sungbintool.ToastUtils
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +16,8 @@ class MainActivity : AppCompatActivity() {
             ToastUtils.show(applicationContext,
             "라이브러리 테스트!", ToastUtils.SUCCESS,
             ToastUtils.SHORT)
-
+            PermissionUtils.request(this,
+                "테스트!", arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE))
         }
     }
 }
