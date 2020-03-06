ca8a0dff9b3987ca400b57b16899e85c85be3f05
SungBin5304 <sungbin.dev@gmail.com>
2020-03-06 21:38:22 +0900

    Update

--- SungBinTool/src/main/java/com/sungbin/sungbintool/PermissionUtils.kt
+++ SungBinTool/src/main/java/com/sungbin/sungbintool/PermissionUtils.kt
@@ -1,17 +1,20 @@
 package com.sungbin.sungbintool
 
-import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.DialogInterface
-import android.content.pm.PackageManager
+import android.content.Intent
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+
 
 object PermissionUtils {
     fun request(act: Activity, message: String, permission: Array<String>){
         DialogUtils.show(act, "권한 필요", message, DialogInterface.OnClickListener { _, _ ->
             ActivityCompat.requestPermissions(act, permission, 1);
         })
+    }
+
+    fun requestReadNotification(act: Activity){
+        val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+        act.startActivity(intent)
     }
 }
