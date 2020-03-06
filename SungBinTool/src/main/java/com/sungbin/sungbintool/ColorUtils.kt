3e1c9104f72e2da5c0cc058ea066601e222f7898
SungBin5304 <sungbin.dev@gmail.com>
2020-03-06 21:59:45 +0900

    Update

--- SungBinTool/src/main/java/com/sungbin/sungbintool/ColorUtils.kt
+++ SungBinTool/src/main/java/com/sungbin/sungbintool/ColorUtils.kt
@@ -1,7 +1,9 @@
 package com.sungbin.sungbintool
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Build
+import android.view.View
 import android.view.WindowManager
 
 object ColorUtils {
@@ -11,6 +13,15 @@ object ColorUtils {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.statusBarColor = color
+        }
+    }
+
+    fun setStatusBarIconColorGray(act: Activity){
+        val view = act.window.decorView
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else if (Build.VERSION.SDK_INT >= 21) {
+            act.window.statusBarColor = Color.BLACK
         }
     }
     
