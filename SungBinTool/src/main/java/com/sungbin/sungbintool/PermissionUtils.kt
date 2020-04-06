bc44a1e7ce70e337e2e0a611c010791a863e6522
SungBin5304 <sungbin.dev@gmail.com>
2020-04-07 07:26:55 +0900

    Fix Bug

--- SungBinTool/src/main/java/com/sungbin/sungbintool/PermissionUtils.kt
+++ SungBinTool/src/main/java/com/sungbin/sungbintool/PermissionUtils.kt
@@ -3,14 +3,24 @@ package com.sungbin.sungbintool
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 object PermissionUtils {
     fun request(act: Activity, message: String, permission: Array<String>){
-        DialogUtils.show(act, "권한 필요", message, DialogInterface.OnClickListener { _, _ ->
-            ActivityCompat.requestPermissions(act, permission, 1);
-        })
+        var isShowDialog = false
+        for(element in permission){
+            if(ContextCompat.checkSelfPermission(act, element) == PackageManager.PERMISSION_DENIED){
+                isShowDialog = true
+            }
+        }
+        if(isShowDialog) {
+            DialogUtils.show(act, "권한 필요", message, DialogInterface.OnClickListener { _, _ ->
+                ActivityCompat.requestPermissions(act, permission, 1);
+            })
+        }
     }
 
     fun requestReadNotification(act: Activity){
