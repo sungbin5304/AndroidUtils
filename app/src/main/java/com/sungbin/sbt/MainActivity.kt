49a38ffe95ab0c5d2b5ba3a881fd75e42d11a24f
SungBin5304 <sungbin.dev@gmail.com>
2020-05-03 22:14:40 +0900

    Update TagableRoundImageView View.

--- app/src/main/java/com/sungbin/sbt/MainActivity.kt
+++ app/src/main/java/com/sungbin/sbt/MainActivity.kt
@@ -12,12 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fab.setOnClickListener {
-            ToastUtils.show(applicationContext,
-            "라이브러리 테스트!", ToastUtils.SUCCESS,
-            ToastUtils.SHORT)
-            PermissionUtils.request(this,
-                "테스트!", arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE))
-        }
     }
 }
