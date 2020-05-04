e97a17f98b02fbcf1cdcf92ad315289809dd2277
SungBin5304 <sungbin.dev@gmail.com>
2020-05-04 14:05:25 +0900

    Update DialogUtils cancelable parameter

--- library/src/main/java/com/sungbin/sungbintool/DialogUtils.kt
+++ library/src/main/java/com/sungbin/sungbintool/DialogUtils.kt
@@ -6,18 +6,20 @@ import android.content.DialogInterface
 
 object DialogUtils {
     fun show(ctx: Context, title: String, message: String,
-             listener: DialogInterface.OnClickListener?){
+             listener: DialogInterface.OnClickListener?, cancelable: Boolean = true){
         val dialog = AlertDialog.Builder(ctx)
         dialog.setTitle(title)
         dialog.setMessage(message)
+        dialog.setCancelable(cancelable)
         dialog.setPositiveButton(ctx.getString(R.string.string_ok), listener)
         dialog.show()
     }
 
     fun showOnce(ctx: Context, title: String, message: String,
-                 id: String, listener: DialogInterface.OnClickListener?){
+                 id: String, listener: DialogInterface.OnClickListener?,
+                 cancelable: Boolean = true){
         if(!DataUtils.readData(ctx, "$id - dialog", "false").toBoolean()){
-            show(ctx, title, message, listener)
+            show(ctx, title, message, listener, cancelable)
             DataUtils.saveData(ctx, "$id - dialog", "true")
         }
     }
