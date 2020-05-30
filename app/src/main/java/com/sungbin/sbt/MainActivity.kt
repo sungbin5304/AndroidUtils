fbbeccc6830b5c81d7bcc4b55a4c2e8019ab2881
SungBin5304 <sungbin.dev@gmail.com>
2020-05-31 05:24:54 +0900

    update LogUtils

--- app/src/main/java/com/sungbin/sbt/MainActivity.kt
+++ app/src/main/java/com/sungbin/sbt/MainActivity.kt
@@ -2,15 +2,32 @@ package com.sungbin.sbt
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.sungbin.sungbintool.LogUtils
 import com.sungbin.sungbintool.PermissionUtils
 import com.sungbin.sungbintool.ToastUtils
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "TAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val map = HashMap<String, String>()
+        map["A"] = "ABC"
+        map["B"] = "BCD"
+        map["C"] = "CDE"
+        LogUtils.d(map)
+
+        LogUtils.d(arrayListOf("T", "E", "S", "T"))
+
+        LogUtils.d("오!! 이쁜로그!!")
+
+        Log.d(TAG, "일반로그")
     }
 }
