ac291de4ea8bd7789ebdecec968f1aac081e2933
sungbin5304 <sungbin.dev@gmail.com>
2020-08-06 16:27:49 +0900

    :rabbit2: update extensions and dimens :rabbit2:

--- library/src/main/java/com/sungbin/sungbintool/extensions/Extensions.kt
+++ library/src/main/java/com/sungbin/sungbintool/extensions/Extensions.kt
@@ -1,12 +1,52 @@
 package com.sungbin.sungbintool.extensions
 
+import android.annotation.SuppressLint
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.annotation.IdRes
+import com.sungbin.sungbintool.StringUtils
 
 /**
  * Created by SungBin on 2020-06-10.
  */
+
+fun View.hide(isGone: Boolean = false){
+    this.visibility = if (isGone) View.GONE else View.INVISIBLE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+operator fun TextView.plusAssign(text: String) {
+    this.text = text
+}
+
+fun TextView.clear() {
+    this.text = ""
+}
+
+operator fun View.get(@IdRes id: Int) = this.findViewById<View>(id)!!
+
+@SuppressLint("ClickableViewAccessibility")
+fun EditText.setEndDrawableClickEvent(action: (View) -> Unit){
+    this.setOnTouchListener(View.OnTouchListener { view, event ->
+        if (event.action == MotionEvent.ACTION_UP) {
+            if (event.rawX >= this.right - this.compoundDrawables[2].bounds.width()
+            ) {
+                action(view)
+                return@OnTouchListener true
+            }
+        }
+        false
+    })
+}
+
+fun String?.toEditable() = StringUtils.toEditable(this.toString())
 
 interface TextWatcherListener : TextWatcher {
     override fun afterTextChanged(s: Editable?) {}
@@ -37,6 +77,3 @@ fun EditText.onTextChanged(action: (s: CharSequence?, start: Int, before: Int, c
         }
     })
 }
-
-fun EditText.clear() = this.setText("")
-
