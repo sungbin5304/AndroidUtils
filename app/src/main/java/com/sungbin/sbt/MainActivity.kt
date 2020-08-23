80259dfe607dbc178125f1a6bf6e98a6331d632f
sungbin5304 <sungbin.dev@gmail.com>
2020-08-23 20:00:06 +0900

    fix error

--- app/src/main/java/com/sungbin/sbt/MainActivity.kt
+++ app/src/main/java/com/sungbin/sbt/MainActivity.kt
@@ -3,9 +3,6 @@ package com.sungbin.sbt
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sungbin.sungbintool.LogUtils
-import com.sungbin.sungbintool.extensions.afterTextChanged
-import com.sungbin.sungbintool.extensions.clear
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
