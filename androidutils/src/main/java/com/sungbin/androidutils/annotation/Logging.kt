5c3287d3672ee17dc596be485696b1252216d67b
sungbin5304 <sungbin.dev@gmail.com>
2020-12-04 12:28:31 +0900

    :bear: Update Extensions :bear:

--- androidutils/src/main/java/com/sungbin/androidutils/annotation/Logging.kt
+++ androidutils/src/main/java/com/sungbin/androidutils/annotation/Logging.kt
@@ -2,6 +2,5 @@ package com.sungbin.androidutils.annotation
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-@MustBeDocumented
 @Repeatable
 annotation class Logging // todo
