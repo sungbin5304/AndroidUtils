e160134b2b5eddb5f5d5df6b766c218525f72914
sungbin5304 <sungbin.dev@gmail.com>
2021-01-15 21:04:14 +0900

    testing

---
+++ annotation/src/main/java/me/sungbin/androidutils/annotation/GreetingGenerator.kt
@@ -0,0 +1,6 @@
+package me.sungbin.androidutils.annotation
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class GreetingGenerator
--- app/src/main/java/com/sungbin/sbt/MainActivity.kt
+++ app/src/main/java/com/sungbin/sbt/MainActivity.kt
@@ -8,14 +8,19 @@ import com.sungbin.androidutils.annotation.ContextChecker
 import com.sungbin.androidutils.annotation.ContextType
 import com.sungbin.androidutils.annotation.Logging
 import com.sungbin.androidutils.util.Logger
+import me.sungbin.androidutils.annotation.GreetingGenerator
 
 @Logging
 class MainActivity : AppCompatActivity() {
+
+    @GreetingGenerator
+    class Santa
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         checkContextType(applicationContext)
+        println(Generated_Santa().greeting())
     }
 
     private fun checkContextType(@ContextChecker(ContextType.APPLICATION) context: Context) {
