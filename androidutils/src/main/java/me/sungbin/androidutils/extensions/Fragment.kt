f3ef9d5d22881c521a6666423a9ca1f7cd4ba71f
sungbin5304 <sungbin.dev@gmail.com>
2021-01-30 00:40:25 +0900

    update

--- androidutils/src/main/java/me/sungbin/androidutils/extensions/Fragment.kt
+++ androidutils/src/main/java/me/sungbin/androidutils/extensions/Fragment.kt
@@ -1,15 +1,57 @@
 package me.sungbin.androidutils.extensions
 
 import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import me.sungbin.androidutils.util.toastutil.ToastLength
 import me.sungbin.androidutils.util.toastutil.ToastType
 import me.sungbin.androidutils.util.toastutil.ToastUtil
+import java.io.Serializable
 
 /**
  * Created by SungBin on 2021-01-20.
  */
+
+@Throws(Exception::class)
+inline fun <reified T> Fragment.startActivity(
+    isNewTask: Boolean = false,
+    vararg extras: Pair<String, *>
+) {
+    requireActivity().startActivity(
+        Intent(requireActivity(), T::class.java).apply {
+            if (isNewTask) addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            extras.iterator().forEach {
+                when (it.second) {
+                    is Int -> putExtra(it.first, it.second as Int)
+                    is Long -> putExtra(it.first, it.second as Long)
+                    is Char -> putExtra(it.first, it.second as Char)
+                    is Byte -> putExtra(it.first, it.second as Byte)
+                    is Short -> putExtra(it.first, it.second as Short)
+                    is Float -> putExtra(it.first, it.second as Float)
+                    is Double -> putExtra(it.first, it.second as Double)
+                    is Boolean -> putExtra(it.first, it.second as Boolean)
+                    is String? -> putExtra(it.first, it.second as String?)
+                    is Bundle? -> putExtra(it.first, it.second as Bundle?)
+                    is IntArray? -> putExtra(it.first, it.second as IntArray?)
+                    is CharArray? -> putExtra(it.first, it.second as CharArray?)
+                    is LongArray? -> putExtra(it.first, it.second as LongArray?)
+                    is ByteArray? -> putExtra(it.first, it.second as ByteArray?)
+                    is ShortArray? -> putExtra(it.first, it.second as ShortArray?)
+                    is Parcelable? -> putExtra(it.first, it.second as Parcelable?)
+                    is FloatArray? -> putExtra(it.first, it.second as FloatArray?)
+                    is DoubleArray? -> putExtra(it.first, it.second as DoubleArray?)
+                    is CharSequence? -> putExtra(it.first, it.second as CharSequence?)
+                    is Serializable? -> putExtra(it.first, it.second as Serializable?)
+                    is BooleanArray? -> putExtra(it.first, it.second as BooleanArray?)
+                    else -> throw Exception("${it.first} pair second value(${it.second}) is not supported type.")
+                }
+            }
+        }
+    )
+}
 
 fun Fragment.alert(
     title: String?,
--- app/src/main/java/com/sungbin/sbt/TestRoom.kt
+++ app/src/main/java/com/sungbin/sbt/TestRoom.kt
@@ -2,7 +2,7 @@ package com.sungbin.sbt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import me.sungbin.androidutils.extensions.toast
+import me.sungbin.androidutils.extensions.startActivity
 
 /**
  * Created by SungBin on 2021-01-16.
@@ -12,6 +12,6 @@ class TestRoom : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-        toast("AA")
+        startActivity<MainActivity>(false, "test" to "BBB")
     }
 }
